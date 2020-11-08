bd5ea991d4a1cf54f0c7de57be603f2e41ac6595
jia <sunjaekko>
2020-11-08 14:29:01 +0800

    first commit

---
+++ src/main/java/com/qf/HelloGit.java
@@ -0,0 +1,11 @@
+package com.qf;
+
+/**
+ * @author sunjiajia
+ * @create 2020-11-08 13:24
+ */
+public class HelloGit {                                             
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
