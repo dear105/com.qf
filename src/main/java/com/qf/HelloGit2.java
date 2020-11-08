61b94b41259819de9bfb62c17eb764d76cd42a59
jia <sunjaekko>
2020-11-08 14:49:26 +0800

    A over in dev

---
+++ src/main/java/com/qf/HelloGit2.java
@@ -0,0 +1,12 @@
+package com.qf;
+
+/**
+ * @author sunjiajia
+ * @create 2020-11-08 14:30
+ */
+public class HelloGit2 {
+    public static void main(String[] args) {
+        System.out.println("hello git22");
+        System.out.println("haha");
+    }
+}
--- src/main/java/com/qf/HelloGit3.java
+++ src/main/java/com/qf/HelloGit3.java
@@ -2,11 +2,10 @@ package com.qf;
 
 /**
  * @author sunjiajia
- * @create 2020-11-08 13:24
+ * @create 2020-11-08 14:48
  */
-public class HelloGit {                                             
+public class HelloGit3 {
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("world");
     }
 }
